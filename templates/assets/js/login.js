2d3410fbd1b47b96b9e1e0ff78325fb68d0a054a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-10 13:26:23 -0300

    simple index validation update

--- templates/assets/js/login.js
+++ templates/assets/js/login.js
@@ -1,3 +1,13 @@
 function developing() {
     alert('Ainda esta em desenvolvimento')
 }
+
+function loginValidation() {
+    let email_adress = document.querySelector('input#emailInput')
+    let password = document.querySelector('input#passwordInput')
+    if (email_adress.value.length == '' || password.value.length == '') {
+        alert('digite valores validos')
+    } else if (password.value.length < 8) {
+        alert('Por favor digite uma senha com 8 ou mais caracteres')
+    }
+}
