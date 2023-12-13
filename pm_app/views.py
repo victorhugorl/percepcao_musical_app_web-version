50c5452c75fe3ea3cd0b39062961ccc23e0e2071
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-12-13 18:50:21 -0300

    adicionando arquivos estáticos e atualizando templates

--- pm_app/views.py
+++ pm_app/views.py
@@ -5,31 +5,51 @@ from django.shortcuts import render
 
 def home(request):
 
+    context = {
+        'title': 'Home'
+    }
+
     return render(
         request,
-        'pm_app/index.html'
+        'pm_app/index.html',
+        context,
     )
 
 
 def login(request):
 
+    context = {
+        'title': 'Login'
+    }
+
     return render(
         request,
-        'pm_app/login.html'
+        'pm_app/login.html',
+        context,
     )
 
 
 def main(request):
 
+    context = {
+        'title': 'Menu Principal'
+    }
+
     return render(
         request,
-        'pm_app/main.html'
+        'pm_app/main.html',
+        context
     )
 
 
 def game(request):
 
+    context = {
+        'title': 'Treinamento'
+    }
+
     return render(
         request,
-        'pm_app/game.html'
+        'pm_app/game.html',
+        context
     )
