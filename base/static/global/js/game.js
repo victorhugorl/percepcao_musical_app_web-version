1addf16866847ca7d3721682804cbf2e22a5b9a4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-31 12:37:44 -0300

    Estou cansado chefe

--- base/static/global/js/game.js
+++ base/static/global/js/game.js
@@ -1,4 +1,5 @@
-import {createQuestions}from '';
+import { createQuestions } from "./modules/questions";
+
 class Timer {
     constructor(clock) {
         this.clock = clock;
@@ -9,62 +10,54 @@ class Timer {
 
     createHourFromSeconds = function (seconds) {
         const data = new Date(seconds * 1000);
-        return data.toLocaleTimeString('pt-BR', {
+        return data.toLocaleTimeString("pt-BR", {
             hour12: false,
-            timeZone: 'UTC'
+            timeZone: "UTC"
         });
-    }
+    };
 
     runClock = () => {
         this.timer = setInterval(() => {
             this.seconds++;
             this.clock.innerHTML = this.createHourFromSeconds(this.seconds);
         }, 1000);
-    }
+    };
 
     start = () => {
-        this.clock.classList.remove('paused');
+        this.clock.classList.remove("paused");
         clearInterval(this.timer);
         this.runClock();
-    }
+    };
 
     pause = () => {
         clearInterval(this.timer);
-        this.clock.classList.add('paused');
-    }
+        this.clock.classList.add("paused");
+    };
 
     reset = () => {
         clearInterval(this.timer);
-        this.clock.innerHTML = '00:00:00';
-        this.clock.classList.remove('paused');
+        this.clock.innerHTML = "00:00:00";
+        this.clock.classList.remove("paused");
         this.seconds = 0;
     };
 }
 
-// Nome das notas 
-
-
-
-
-
+// Nome das notas
 
 // Definindo lugares para trabalhar
-const clock = document.querySelector('.clock');
-const textDisplay = document.querySelector('.text-display');
-const continueOrSkip = document.querySelector('.continue-skip');
-const divButtons = document.querySelector('.buttons-div');
+const clock = document.querySelector(".clock");
+const textDisplay = document.querySelector(".text-display");
+const continueOrSkip = document.querySelector(".continue-skip");
+const divButtons = document.querySelector(".buttons-div");
 
-
-
-textDisplay.innerHTML = 'Qual é a nota tocada ?'
+textDisplay.innerHTML = "Qual é a nota tocada ?";
 
 console.log(divButtons);
 
-
 // Audio
 
-audio = document.getElementById('my-audio');
-console.log(audio)
+audio = document.getElementById("my-audio");
+console.log(audio);
 audio.currentTime = 0;
 audio.play();
 
@@ -72,15 +65,27 @@ audio.play();
 cron = new Timer(clock);
 cron.runClock();
 
-console.log(continueOrSkip)
-continueOrSkip.addEventListener('click' , event => {
-    audio.play()
-})
+console.log(continueOrSkip);
+continueOrSkip.addEventListener("click", (event) => {
+    audio.play();
+});
 
+console.log(" está vindo para cá ?");
 
-console.log(' está vindo para cá ?');
+let note_list = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "A#",
+    "C#",
+    "D#",
+    "F#",
+    "G#"
+];
 
-let note_list = ['A','B','C','D','E','F','G', 'A#','C#','D#','F#','G#'];
-
-console.log(createQuestions(note_list, 4))
-
+console.log(createQuestions(note_list, 4));
+console.log("Funciona essa mizera omi");
