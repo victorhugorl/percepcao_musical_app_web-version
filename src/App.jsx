2337a21eebb9ac89cff5b4925945ee88e5530584
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-01 15:22:11 -0300

    Small update

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import React from "react";
 import "./assets/css/style.css";
 
 import Index from "./pages/home/Index";
-// import Login from "./pages/home/Login";
-// import Main from "./pages/home/Main";
 
 export default () => {
     return (
         <>
             <Index />
-            {/* <Login /> */}
-            {/* <Main /> */}
         </>
     );
 };
