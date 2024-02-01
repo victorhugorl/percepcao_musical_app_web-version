532fcff9f130eb4b8f4c869b935656ed912de4b7
victor <victorlimasg@email.com>
2024-02-01 17:54:51 +0000

    resetando a configuração do vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true },
-  base: process.env.mode === "production" ? "/static/" : "/",
-  root: "./src",
-  plugins: [reactRefresh()],
-});
+  plugins: [react()],
+})
+
+
 
