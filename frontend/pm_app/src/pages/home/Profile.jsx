b8018222d398383fc803d1fd32890362c3f2a002
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-06 11:41:07 -0300

    Bug resolved and creating features for react

--- frontend/pm_app/src/pages/home/Profile.jsx
+++
@@ -1,7 +0,0 @@
-export default () => {
-    return (
-        <>
-            <h1>Sua conta kk</h1>
-        </>
-    );
-};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Index from "./pages/home/Index.jsx";
+import Login from "./pages/home/Login.jsx";
+import Main from "./pages/home/Main.jsx";
+import Profile from "./pages/home/Profile.jsx";
+import Game from "./pages/game/Game.jsx";
+
+// vou importar aqui o boostrap e Css
+
+import "./assets/css/style.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Index />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/main",
+        element: <Main />
+    },
+    {
+        path: "/profile",
+        element: <Profile />
+    },
+    {
+        path: "/main/game",
+        element: <Game />
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        {/* <App /> */}
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
