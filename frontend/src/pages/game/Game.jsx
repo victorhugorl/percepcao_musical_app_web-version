ee1d5adcdaeec474bd68ad7db63be007acb2fb23
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-06 14:06:16 -0300

    using time from web

--- frontend/src/pages/game/Game.jsx
+++ frontend/src/pages/game/Game.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import Header from "../../components/Header";
 
@@ -8,19 +8,66 @@ export default () => {
     // Nome das notas
 
     // Definindo lugares para trabalhar
+
     const [textDisplay, setTextDisplay] = useState("Vai começar");
     const textSet = () => {
         setTextDisplay("Começou kk");
-        setClocl("00:05:00");
     };
 
-    const [clock, setClocl] = useState("00:00:00");
-    const timerChange = () => {
-        setTextDisplay("Se prepara ai boi vei");
-        setClocl("00:00:00");
+    const Ref = useRef(null);
+
+    const [timer, setTimer] = useState("00:00:00");
+
+    const getTimeRemaining = (e) => {
+        const total = Date.parse(e) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+        return {
+            total,
+            hours,
+            minutes,
+            seconds
+        };
     };
 
-    // para não poluir apaguei alguns comentarios o arquivo js ainda esta em assets
+    const startTimer = (e) => {
+        let { total, hours, minutes, seconds } = getTimeRemaining(e);
+        if (total >= 0) {
+            setTimer(
+                (hours > 9 ? hours : "0" + hours) +
+                    ":" +
+                    (minutes > 9 ? minutes : "0" + minutes) +
+                    ":" +
+                    (seconds > 9 ? seconds : "0" + seconds)
+            );
+        }
+    };
+
+    const clearTimer = (e) => {
+        setTimer("00:00:50");
+
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e);
+        }, 1000);
+        Ref.current = id;
+    };
+
+    const getDeadTime = () => {
+        let deadline = new Date();
+
+        deadline.setSeconds(deadline.getSeconds() + 50);
+        return deadline;
+    };
+
+    useEffect(() => {
+        clearTimer(getDeadTime());
+    }, []);
+
+    const onClickReset = () => {
+        clearTimer(getDeadTime());
+    };
 
     return (
         <>
@@ -95,12 +142,12 @@ export default () => {
                         <div className="d-flex justify-content-between p-4">
                             <span
                                 className="btn btn-light px-5 btn-lg"
-                                onClick={timerChange}
+                                onClick={onClickReset}
                             >
                                 repetir
                             </span>
                             <span className="btn btn-outline-light disabled px-5 btn-lg">
-                                {clock}
+                                {timer}
                             </span>
                             {/* TESTE AQUI PARA MUDAR TEXTO */}
                             <span
