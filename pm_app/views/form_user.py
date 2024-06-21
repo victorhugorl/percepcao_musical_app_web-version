5b8d870697f66ea68df28d4e1c694327fb443e5d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-06-21 09:14:25 -0300

    config core project again new form init creating register user

--- pm_app/views/form_user.py
+++ pm_app/views/form_user.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
+def user_view(request, name_user):
+    context = {
+        'title': f'{name_user} - PMA'
+    }
+    return render(request, 'pm_app/pages/user_page/profile.html', context)
+
 def login(request):
     context = {
         'title': 'Login - PMA'
     }
 
-    return render(request, 'pm_app/form_pages/login.html', context)
+    return render(request, 'pm_app/pages/form_pages/login.html', context)
 
 def register(request):
     context = {
-        'title': 'Login - PMA'
+        'title': 'Registrar - PMA'
     }
 
-    return render(request, 'pm_app/form_pages/login.html', context)
+    return render(request, 'pm_app/pages/form_pages/register.html', context)
