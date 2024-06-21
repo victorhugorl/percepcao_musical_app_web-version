eb2a12d836897740938bd5b8a06a803b7673aa1e
victor <victorlimasg@email.com>
2024-06-21 17:53:50 -0300

    criando classe de inicialização do jogo

--- base/static/global/js/game.js
+++ base/static/global/js/game.js
@@ -40,45 +40,90 @@ class Timer {
     };
 }
 
-// Nome das notas
-data = document.currentScript.dataset;
-questions = data.questions;
+class App {
 
-// Definindo lugares para trabalhar
-const modal = new bootstrap.Modal(document.getElementById('confirm'));
-const confirmBtn = document.querySelector(".confirm");
+    constructor (){
+            // Nome das notas
+        this.data = document.currentScript.dataset;
+        this.questions = this.data.questions;
+            
+        // Definindo lugares para trabalhar
+        this.modal = new bootstrap.Modal(document.getElementById('confirm'));
+        this.confirmBtn = document.querySelector(".confirm");
+        this.clock = document.querySelector(".clock");
+        this.textDisplay = document.querySelector(".text-display");
+        this.continueOrSkip = document.querySelector(".continue-skip");
+        this.divButtons = document.querySelector(".buttons-div");
+        this.repeatButton = document.querySelector('.repeat')
+        this.cron = new Timer(this.clock);
 
-const clock = document.querySelector(".clock");
-const textDisplay = document.querySelector(".text-display");
-const continueOrSkip = document.querySelector(".continue-skip");
-const divButtons = document.querySelector(".buttons-div");
+    }
+
+    startApp = () => {
+        
+        this.showPopup() // mostra popup de confirmação
+        this.confirmBtn.onclick = () => { //função que fecha o popup 
+            this.modal.hide() 
+            this.startRound() // inicia uma rodada do jogo
+
+        }
+        
+    }
+
+    showPopup = () => {
+        window.onload = function () {
+            this.modal.show()
+         
+        };        
+    }
+
+    startRound = () => {
+        this.textDisplay.innerHTML = "Qual é a nota tocada ?";
+        this.continueOrSkip.innerHTML = 'Pular'
+        
+
+    }
+
+}
 
 
-window.onload = function () {
-    modal.show()
- 
-};
-
-confirmBtn.onclick = function () {
-   modal.hide()
-   audio.play()
-   cron.runClock();
 
 
-    // executar o que eu quizer
-};
 
-textDisplay.innerHTML = "Qual é a nota tocada ?";
+
+
+  
+
 
 // Audio
 
-audio = document.getElementById("my-audio");
-audio.currentTime = 0;
-
-// Iniciando cronometro
-cron = new Timer(clock);
 
 
-continueOrSkip.addEventListener("click", (event) => {
-    audio.play();
-});
+
+//repeatButton.addEventListener("click", (event) =>{
+  //  audio.currentTime = 0;
+    //audio.play();
+//})
+
+
+// for(let  i = 0; i < divButtons.children.length; i++){
+
+//    console.log(divButtons.children[i].tagName)
+//    if(divButtons.children[i].tagName == "SPAN") {
+//        let button = divButtons.children[i]
+        
+//        button.addEventListener('click', (event) => {
+ //           console.log('estou clicando no botao ')
+ //           console.log(button.innerHTML)
+ //           let selectedOption = button.innerHTML.trim();
+ //           console.log(button);
+ //           checkAnswer(selectedOption, button );
+ //       })
+        
+ //   }
+
+//}
+
+myApp = new App()
+
+myApp.startApp()
