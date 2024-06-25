ab64f8f0669f57dbf17ccf3e27b6943e4446f996
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-06-25 10:47:27 -0300

    new feature and making small design

--- pm_app/views/index_views.py
+++ pm_app/views/index_views.py
@@ -1,5 +1,6 @@
+from pm_app.models import Profile
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 def index(request):
 
@@ -14,8 +15,10 @@ def index(request):
 
 @login_required(login_url='pm_app:login')
 def main(request):
+    profile = get_object_or_404(Profile, user=request.user)
     context = {
-        'title': 'Menu Principal - PMA'
+        'title': 'Menu Principal - PMA',
+        'profile': profile
     }
 
     return render(request, 'pm_app/main.html', context)
