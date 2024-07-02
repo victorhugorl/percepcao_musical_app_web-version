0478f85ee0c9aabda5886f1d91856a6c0c6c4e2f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-02 13:56:49 -0300

    changing migrations and models

--- pm_app/models/user_model.py
+++ pm_app/models/user_model.py
@@ -28,24 +28,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
-
-class Note (models.Model):
-    note = models.CharField(max_length=20)
-    audio_file = models.URLField(max_length=200)
-    
-
-class Chord (models.Model):
-    chord = models.CharField(max_length=60)
-    audio_file = models.URLField(max_length=200)
-
-
-class GameType (models.Model):
-    game_type = models.CharField(max_length=50)
-
-
-
-class Game(models.Model):
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
-    game_type = models.ForeignKey(GameType, on_delete=models.SET_NULL, blank=True, null=True)
-    completed = models.BooleanField(default=False)
-    ended_at = models.DateField(default='')
