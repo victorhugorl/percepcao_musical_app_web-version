7cf07fe4e04d2b657d2e94c03712c3e63de18002
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-03 12:36:32 -0300

    creating index

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -132,7 +132,7 @@ export default () => {
                     </div>
                 </div>
             </div>
-            <div className="py-5 container" id="icon-grid">
+            <div className="container" id="icon-grid">
                 <h2 className="pb-2 border-bottom">O que temos de novo?</h2>
 
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
@@ -221,16 +221,10 @@ export default () => {
                         </div>
                     </div>
                     <div className="col d-flex align-items-start">
-                        <svg
-                            className="bi text-body-secondary flex-shrink-0 me-3"
-                            width="1.75em"
-                            height="1.75em"
-                        >
-                            <use xlinkActuate="#tools"></use>
-                        </svg>
+                        <i className="bi bi-plus fs-2 me-3"></i>
                         <div>
                             <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
-                                Featured title
+                                Inovação
                             </h3>
                             <p>
                                 Paragraph of text beneath the heading to explain
