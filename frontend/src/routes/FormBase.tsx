c9864d067429fc1fe6811af813d747eb4c519700
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-04 13:48:16 -0300

    testando api

--- frontend/src/routes/FormBase.tsx
+++ frontend/src/routes/FormBase.tsx
@@ -38,7 +38,7 @@ export default () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("https://api.example.com/endpoint", {
+            const response = await fetch("http://127.0.0.1:8000/api//", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
